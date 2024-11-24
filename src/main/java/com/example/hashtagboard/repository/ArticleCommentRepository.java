772a51f9dcf41dcc7ccf10bde172ffef97a0de56
LEEDAEJIN <ldj4241@naver.com>
2024-11-24 23:05:57 +0900

    data rest 설정

--- src/main/java/com/example/hashtagboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/hashtagboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,9 @@ package com.example.hashtagboard.repository;
 
 import com.example.hashtagboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
