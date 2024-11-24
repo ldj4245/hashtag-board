88659c283328e122c0a9c2cb80b833f19054a182
LEEDAEJIN <ldj4241@naver.com>
2024-11-24 17:03:07 +0900

    Initial commit

---
+++ src/main/java/com/example/hashtagboard/HashtagBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.hashtagboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashtagBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashtagBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hashtagboard/HashtagBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hashtagboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashtagBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
